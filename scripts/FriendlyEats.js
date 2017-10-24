ca2559f686bdef45ed5b080ddcbe8dd0bdf46feb
Nicolas Garnier <nivco@google.com>
2017-10-24 12:14:38 +0200

    Fixes #13

--- scripts/FriendlyEats.js
+++ scripts/FriendlyEats.js
@@ -189,18 +189,4 @@ FriendlyEats.prototype.data = {
 
 window.onload = function() {
   window.app = new FriendlyEats();
-
-  if (navigator.serviceWorker) {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then(function(registration) {
-        console.log(
-          "ServiceWorker registration successful with scope:",
-          registration.scope
-        );
-      })
-      .catch(function(error) {
-        console.log("ServiceWorker registration failed:", errror);
-      });
-  }
 };
