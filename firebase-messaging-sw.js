5bb490b3e06317ae8f531cf6b3b2977bdf87204a
David East <deast@google.com>
2022-07-28 10:20:29 -0400

    empty config

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -17,14 +17,7 @@ importScripts('https://www.gstatic.com/firebasejs/9.7.0/firebase-app-compat.js')
 importScripts('https://www.gstatic.com/firebasejs/9.7.0/firebase-messaging-compat.js');
 
 firebase.initializeApp({
-  apiKey: "AIzaSyC4pjYxIa_Mz_ceS5ppLc9dmI9ycqHe_ik",
-  authDomain: "friendly-eats-5c2b4.firebaseapp.com",
-  databaseURL: "https://friendly-eats-5c2b4.firebaseio.com",
-  projectId: "friendly-eats-5c2b4",
-  storageBucket: "friendly-eats-5c2b4.appspot.com",
-  messagingSenderId: "373507430883",
-  appId: "1:373507430883:web:5d3f195256b717739a28d6",
-  measurementId: "G-M86R7EN02K"
+  /* Enter your config here */
 });
 
 firebase.messaging();
