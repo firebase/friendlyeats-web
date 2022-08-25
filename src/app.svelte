3b43c1a7601a717b1ce509acfd04a0b8057e3410
Basuke Suzuki <basuke@mac.com>
2022-08-24 21:24:30 -0700

    Get id from url match.

--- src/app.svelte
+++ src/app.svelte
@@ -9,13 +9,6 @@ export let that = null;
 
 </script>
 
-<nav>
-    <a href="/">Index</a>
-    <a href="/restaurant/taro">Ramen Taro</a>
-    <a href="/restaurant/jiro">Ramen Jiro</a>
-    <a href="/setup">Setup</a>
-</nav>
-
 <Route path="/"><Index {that}/></Route>
-<Route path="/restaurant/:id"><Restaurant {that}/></Route>
+<Route path="/restaurants/:id"><Restaurant {that}/></Route>
 <Route path="/setup"><Setup {that}/></Route>
--- src/routes/restaurant.svelte
+++ src/routes/restaurant.svelte
@@ -1,2 +1,13 @@
-<div class="header"></div>
+<script>
+
+import { meta } from 'tinro';
+
+export let that = null;
+
+const route = meta();
+const id = route.params['id'];
+
+</script>
+
+<div class="header">{id}</div>
 <main></main>
