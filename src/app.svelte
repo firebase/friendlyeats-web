90e9381a3846a4db5a46be7e2b78b50623ff36a8
Basuke Suzuki <basuke@mac.com>
2022-08-25 23:02:41 -0700

    Move app contents into svelte.

--- src/app.svelte
+++ src/app.svelte
@@ -4,10 +4,12 @@ import { Route } from 'tinro';
 import Index from "./routes/index.svelte";
 import Setup from "./routes/setup.svelte";
 import Restaurant from "./routes/restaurant.svelte";
+import FilterDialog from './components/filter-dialog.svelte';
+import AddReview from './components/add-review.svelte';
+import { mountComponent } from './lib/renderer';
+import { addRating } from './lib/firestore';
 import { restrantIsEmpty } from './lib/stores';
 import { router } from 'tinro';
-
-export let that = null;
 
 const isEmpty = restrantIsEmpty();
 isEmpty.subscribe(isEmpty => {
@@ -15,6 +17,71 @@ isEmpty.subscribe(isEmpty => {
         router.goto('/setup');
     }
 });
+
+const initialFilters = {
+    city: '',
+    price: '',
+    category: '',
+    sort: 'Rating'
+};
+
+class FriendlyEats {
+    constructor() {
+        this.filters = {...initialFilters};
+        this.dialogs = {};
+
+        firebase.auth().signInAnonymously().then(() => {
+            this.initReviewDialog();
+            this.initFilterDialog();
+        }).catch(function(err) {
+            console.log(err);
+        });
+    }
+
+    initReviewDialog() {
+        let values = {
+            rating: 0,
+            text: '',
+        };
+
+        let dialog = document.querySelector('#dialog-add-review');
+        mountComponent(dialog, AddReview, { values });
+        this.dialogs.add_review = new mdc.dialog.MDCDialog(dialog);
+
+        this.dialogs.add_review.listen('MDCDialog:accept', function() {
+            var pathname = getCleanPath(document.location.pathname);
+            var id = pathname.split('/')[2];
+
+            addRating(id, {
+                ...values,
+                userName: 'Anonymous (Web)',
+                timestamp: new Date(),
+                userId: firebase.auth().currentUser.uid
+            });
+        });
+    }
+
+    initFilterDialog() {
+        // TODO: Reset filter dialog to init state on close.
+        const dialogEl = document.querySelector('#dialog-filter-all');
+        mountComponent(dialogEl, FilterDialog, { filters: this.filters });
+        this.dialogs.filter = new mdc.dialog.MDCDialog(dialogEl);
+
+        this.dialogs.filter.listen('MDCDialog:accept', () => {
+            router.goto('/');
+        });
+    }
+};
+
+function getCleanPath(dirtyPath) {
+    if (dirtyPath.startsWith('/index.html')) {
+        return dirtyPath.split('/').slice(1).join('/');
+    } else {
+        return dirtyPath;
+    }
+}
+
+let that = new FriendlyEats();
 
 </script>
 
--- src/main.js
+++ src/main.js
@@ -14,77 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-'use strict';
 
 import App from './app.svelte'; 
-import FilterDialog from './components/filter-dialog.svelte';
-import AddReview from './components/add-review.svelte';
-import { mountComponent } from './lib/renderer';
-import { addRating } from './lib/firestore';
 
-
- /**
-  * Initializes the FriendlyEats app.
-  */
- function FriendlyEats() {
-   this.filters = {
-     city: '',
-     price: '',
-     category: '',
-     sort: 'Rating'
-   };
- 
-   this.dialogs = {};
-
-   var that = this;
-   firebase.auth().signInAnonymously().then(function() {
-     mountComponent(document.querySelector('#app'), App, {that});
-     that.initReviewDialog();
-     that.initFilterDialog();
-   }).catch(function(err) {
-     console.log(err);
-   });
- }
- 
- FriendlyEats.prototype.getCleanPath = function(dirtyPath) {
-   if (dirtyPath.startsWith('/index.html')) {
-     return dirtyPath.split('/').slice(1).join('/');
-   } else {
-     return dirtyPath;
-   }
- };
- 
-FriendlyEats.prototype.initReviewDialog = function() {
-  let values = {
-    rating: 0,
-    text: '',
-  };
-
-  var dialog = document.querySelector('#dialog-add-review');
-  mountComponent(dialog, AddReview, { values });
-  this.dialogs.add_review = new mdc.dialog.MDCDialog(dialog);
-
-  var that = this;
-  this.dialogs.add_review.listen('MDCDialog:accept', function() {
-    var pathname = that.getCleanPath(document.location.pathname);
-    var id = pathname.split('/')[2];
-
-    addRating(id, {
-      ...values,
-      userName: 'Anonymous (Web)',
-      timestamp: new Date(),
-      userId: firebase.auth().currentUser.uid
-    });
-  });
-};
-
-FriendlyEats.prototype.initFilterDialog = function() {
-  // TODO: Reset filter dialog to init state on close.
-  const dialogEl = document.querySelector('#dialog-filter-all');
-  mountComponent(dialogEl, FilterDialog, { filters: this.filters });
-  this.dialogs.filter = new mdc.dialog.MDCDialog(dialogEl);
-
-  this.dialogs.filter.listen('MDCDialog:accept', () => this.viewList() );
-};
-
-window.app = new FriendlyEats();
+new App({target: document.querySelector('#app')});
