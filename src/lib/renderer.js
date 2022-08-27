b25fe568da0c259f637ffd5de87e5b104eca262e
Basuke Suzuki <basuke@mac.com>
2022-08-27 11:40:24 -0700

    Delete unused function and file

--- src/lib/renderer.js
+++
@@ -1,7 +0,0 @@
-export function mountComponent(parent, cls, props = {}) {
-    parent.innerHTML = '';
-    const mounter = document.createElement('div');
-    const component = new cls({target: mounter, props});
-    parent.append(mounter);
-    return component;
-};
