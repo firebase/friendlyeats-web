76b76aaf1d0bb0656357d26f5cbba8d8225cd44f
Basuke Suzuki <basuke@mac.com>
2022-08-27 12:08:08 -0700

    Reduce the noise by adding a wait.

--- src/components/guy.svelte
+++ src/components/guy.svelte
@@ -1,10 +1,11 @@
 <script>
 
+import { fade } from 'svelte/transition';
 export let says = "Bluh bluh bluh";
 
 </script>
 
-<div id="guy-container" class="mdc-toolbar-fixed-adjust">
+<div id="guy-container" class="mdc-toolbar-fixed-adjust" in:fade={{delay: 400, duration: 1}}>
     <img alt="guy says..." class="guy" src="/images/guy_fireats.png" />
     <div class="text">
         {says}
