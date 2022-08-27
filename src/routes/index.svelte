5fd6a803341e471060503ed53842666117c2b53e
Basuke Suzuki <basuke@mac.com>
2022-08-27 11:40:01 -0700

    Move list logic to page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,14 +1,19 @@
 <script>
 
 import { onMount } from 'svelte';
-import { restrantIsEmpty } from '../lib/stores';
 import { router } from 'tinro';
+import { getRestaurants, restrantIsEmpty } from '../lib/stores';
+import RestaurantCard from "../components/restaurant-card.svelte";
 import HeaderBase from '../components/header-base.svelte';
-import ListRestaurants from '../components/list-restaurants.svelte';
 import FilterDialog from '../components/filter-dialog.svelte';
 import Dialog from '../components/dialog.svelte';
 
 export let filters = {};
+
+$: restaurants = getRestaurants(filters);
+
+/* filter dialog */
+
 let filtersEditing;
 let dialogOpened = false;
 
@@ -21,12 +26,16 @@ function onAccept(event) {
     filters = {...filtersEditing};
 }
 
+/* Material Component Toolbar support */
+
 onMount(() => {
     var toolbar = mdc.toolbar.MDCToolbar.attachTo(document.querySelector('.mdc-toolbar'));
     toolbar.fixedAdjustElement = document.querySelector('#message-cards-container');
 
     mdc.autoInit();
 });
+
+/* redirect */
 
 const isEmpty = restrantIsEmpty();
 isEmpty.subscribe(isEmpty => {
@@ -38,7 +47,26 @@ isEmpty.subscribe(isEmpty => {
 </script>
 
 <div class="header"><HeaderBase {filters} on:open-dialog={showFilters}/></div>
-<main><ListRestaurants {filters}/></main>
+<main>
+    <div id="message-cards-container" class="mdc-layout-grid mdc-toolbar-fixed-adjust">
+        {#if $restaurants.length}
+            <div id="cards" class="mdc-layout-grid__inner">
+                {#each $restaurants as doc(doc.id)}
+                    <RestaurantCard id={doc.id} data={doc.data()} />
+                {/each}
+            </div>
+        {:else}
+            <div id="no-results">
+                <div id="guy-container" class="mdc-toolbar-fixed-adjust">
+                    <img class="guy" src="/images/guy_fireats.png" />
+                    <div class="text">
+                        There are no results for your those filters!
+                    </div>
+                </div>
+            </div>
+        {/if}
+    </div>
+</main>
 
 <Dialog bind:opened={dialogOpened} on:accept={onAccept}>
     <FilterDialog filters={filtersEditing}/>
