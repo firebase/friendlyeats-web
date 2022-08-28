48365d49c2fe4eb7ab223c08c0175b85c6b82dd4
Basuke Suzuki <basuke@mac.com>
2022-08-28 12:46:16 -0700

    share static files and rename some files

--- rollup.config.js
+++ rollup.config.js
@@ -34,7 +34,7 @@ export default {
 		sourcemap: true,
 		format: 'iife',
 		name: 'app',
-		file: 'public/build/bundle.js'
+		file: 'static/build/bundle.js'
 	},
 	plugins: [
 		svelte({
@@ -44,7 +44,7 @@ export default {
 				// we'll extract any component CSS out into
 				// a separate file - better for performance
 				css: css => {
-					css.write('public/build/bundle.css');
+					css.write('static/build/bundle.css');
 				},
 			},
 		}),
@@ -64,9 +64,9 @@ export default {
 		// the bundle has been generated
 		!production && serve(),
 
-		// Watch the `public` directory and refresh the
+		// Watch the `static` directory and refresh the
 		// browser on changes when not in production
-		!production && livereload('public'),
+		!production && livereload('static'),
 
 		// If we're building for production (npm run build
 		// instead of npm run dev), minify
