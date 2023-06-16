76f898dc51264cd9ca5596c3311f7e896305dddf
Cynthia Wang <hyacccint@gmail.com>
2023-06-16 14:00:00 +0100

    feat: add app check

--- hosting/src/app.tsx
+++ hosting/src/app.tsx
@@ -9,10 +9,22 @@ import {
     StorageProvider,
     AuthProvider,
     FunctionsProvider,
+    AppCheckProvider,
 } from 'reactfire';
+import {
+    initializeAppCheck,
+    ReCaptchaEnterpriseProvider,
+} from 'firebase/app-check';
+import { APP_CHECK_TOKEN } from './firebase-config';
 import Header from './components/header';
 import Home from './pages/home';
 import Restaurant from './pages/restaurant';
+
+declare global {
+  interface Window {
+    FIREBASE_APPCHECK_DEBUG_TOKEN: boolean;
+  }
+}
 
 function App() {
     const app = useFirebaseApp();
@@ -20,6 +32,10 @@ function App() {
     const storageInstance = getStorage(app);
     const authInstance = getAuth(app);
     const functionsInstance = getFunctions(app);
+    const appCheck = initializeAppCheck(app, {
+        provider: new ReCaptchaEnterpriseProvider(APP_CHECK_TOKEN),
+        isTokenAutoRefreshEnabled: true,
+    });
 
     if (process.env.NODE_ENV !== 'production') {
         // Set up emulators
@@ -29,22 +45,27 @@ function App() {
         });
         connectFirestoreEmulator(firestoreInstance, '127.0.0.1', 8080);
         connectFunctionsEmulator(functionsInstance, '127.0.0.1', 5001);
+        
+        // Enable local testing for App Check
+        self.FIREBASE_APPCHECK_DEBUG_TOKEN = true;
     }
     return (
-        <FirestoreProvider sdk={firestoreInstance}>
-            <StorageProvider sdk={storageInstance}>
-                <AuthProvider sdk={authInstance}>
-                    <FunctionsProvider sdk={functionsInstance}>
-                        <Routes>
-                            <Route path="/" element={<Header />}>
-                                <Route index element={<Home />} />
-                                <Route path="/restaurant/:id" element={<Restaurant />} />
-                            </Route>
-                        </Routes>
-                    </FunctionsProvider>
-                </AuthProvider>
-            </StorageProvider>
-        </FirestoreProvider>
+        <AppCheckProvider sdk={appCheck}>
+            <FirestoreProvider sdk={firestoreInstance}>
+                <StorageProvider sdk={storageInstance}>
+                    <AuthProvider sdk={authInstance}>
+                        <FunctionsProvider sdk={functionsInstance}>
+                            <Routes>
+                                <Route path="/" element={<Header />}>
+                                    <Route index element={<Home />} />
+                                    <Route path="/restaurant/:id" element={<Restaurant />} />
+                                </Route>
+                            </Routes>
+                        </FunctionsProvider>
+                    </AuthProvider>
+                </StorageProvider>
+            </FirestoreProvider>
+        </AppCheckProvider>
     );
 }
 
--- hosting/src/firebase-config.tsx
+++ hosting/src/firebase-config.tsx
@@ -1,3 +1,5 @@
 export const firebaseConfig = {
     // Your Firebase configs
 };
+
+export const APP_CHECK_TOKEN = 'Replace with your recaptcha enterprise site key';
