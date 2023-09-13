2b72ae814d5e3754091e8b776981ec33cf3be31e
Cynthia Wang <44329917+HYACCCINT@users.noreply.github.com>
2023-09-13 17:02:53 -0400

    Next ssr update (#247)
    * feat: environment aware
    
    * fix: update dependencies
    
    * fix: import
    
    * fix: remove package lock
    
    * Delete nextjs-end/.firebase/hinamomori-0x/functions/package-lock.json
    
    * Delete nextjs-end/.firebase/hinamomori-0x/functions/.env
    
    * Delete nextjs-end/.firebase/hinamomori-0x/functions/package.json
    
    * Delete nextjs-end/.firebase/hinamomori-0x/functions/server.js
    
    * fix: typo

--- nextjs-end/src/app/actions.js
+++ nextjs-end/src/app/actions.js
@@ -1,12 +1,17 @@
 "use server";
 
 import { addReviewToRestaurant } from "@/src/lib/firebase/firestore.js";
+import { getAuthenticatedAppForUser } from "@/src/lib/firebase/firebase";
+import { getFirestore } from "firebase/firestore";
 
 // This is a next.js server action, an alpha feature, so
 // use with caution
 // https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions
 export async function handleReviewFormSubmission(data) {
-	await addReviewToRestaurant(data.get("restaurantId"), {
+	const { app } = await getAuthenticatedAppForUser();
+	const db = getFirestore(app);
+
+	await addReviewToRestaurant(db, data.get("restaurantId"), {
 		text: data.get("text"),
 		rating: data.get("rating"),
 
