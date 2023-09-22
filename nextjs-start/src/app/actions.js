0edc61bf07472b2e1adb8d9d30b959d2fdd25308
Umar Hansa <umar.hansa@gmail.com>
2023-09-22 20:53:07 +0100

    Small fixes

--- nextjs-start/src/app/actions.js
+++ nextjs-start/src/app/actions.js
@@ -7,15 +7,6 @@ import { getFirestore } from "firebase/firestore";
 // This is a next.js server action, an alpha feature, so
 // use with caution
 // https://nextjs.org/docs/app/building-your-application/data-fetching/server-actions
-export async function handleReviewFormSubmission(data) {
-	const { app } = await getAuthenticatedAppForUser();
-	const db = getFirestore(app);
 
-	await addReviewToRestaurant(db, data.get("restaurantId"), {
-		text: data.get("text"),
-		rating: data.get("rating"),
-
-		// This came from a hidden form field
-		userId: data.get("userId"),
-	});
-}
+// Replace the function below
+export async function handleReviewFormSubmission(data) {}
