dc2d948dcdcf2aaa86c73abb3e4c0400c4b70b4d
Jeff Huleatt <3759507+jhuleatt@users.noreply.github.com>
2024-05-21 15:04:20 -0400

    implement serverApp in nextjs-end

--- nextjs-start/src/lib/firebase/serverApp.js
+++ nextjs-start/src/lib/firebase/serverApp.js
@@ -9,19 +9,5 @@ import { firebaseConfig } from "./config";
 import { getAuth } from "firebase/auth";
 
 export async function getAuthenticatedAppForUser() {
-  const idToken = headers().get("Authorization")?.split("Bearer ")[1];
-  console.log('firebaseConfig', JSON.stringify(firebaseConfig));
-  const firebaseServerApp = initializeServerApp(
-    firebaseConfig,
-    idToken
-      ? {
-          authIdToken: idToken,
-        }
-      : {}
-  );
-
-  const auth = getAuth(firebaseServerApp);
-  await auth.authStateReady();
-
-  return { firebaseServerApp, currentUser: auth.currentUser };
+  throw new Error('not implemented');
 }
