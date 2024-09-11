6f203ac30c58fed42c0bc1fd2ed33a2ad26989af
James Daniels <jamesdaniels@google.com>
2024-09-11 12:08:42 -0400

    IDX template

---
+++ nextjs-end/idx-template.nix
@@ -0,0 +1,10 @@
+{ pkgs, packageManager ? "npm", ... }: {
+  packages = [
+    pkgs.nodejs_20
+    pkgs.j2cli
+    pkgs.nixfmt
+  ];
+  bootstrap = ''
+    npx -y giget gh:firebase/friendlyeats-web/nextjs-end#jamesdaniels_fixServiceWorkers "$WS_NAME"
+  '';
+}
