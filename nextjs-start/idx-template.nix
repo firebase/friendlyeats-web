be3151a3e0b3dff1f9d4777118f4ff22869587df
James Daniels <jamesdaniels@google.com>
2024-09-11 12:42:32 -0400

    Fix concurrently in idx

---
+++ nextjs-start/idx-template.nix
@@ -0,0 +1,11 @@
+{ pkgs, packageManager ? "npm", ... }: {
+  packages = [
+    pkgs.nodejs_20
+    pkgs.j2cli
+    pkgs.nixfmt
+  ];
+  bootstrap = ''
+    npx -y giget gh:firebase/friendlyeats-web/nextjs-start#jamesdaniels_fixServiceWorkers "$WS_NAME"
+    mv "$WS_NAME" "$out"
+  '';
+}
