63daf2066005feaaf64545b5da47ec754d146320
James Daniels <jamesdaniels@google.com>
2024-09-11 16:00:27 -0400

    IDX setup

--- nextjs-end/.idx/dev.nix
+++ nextjs-end/.idx/dev.nix
@@ -3,10 +3,17 @@
   packages = [
     pkgs.nodejs_20
   ];
+  env = {
+    GOOGLE_PROJECT = "<project-id>";
+    NEXT_TELEMETRY_DISABLED = "1";
+  };
   idx.workspace = {
       onStart = {
         npm-install = "npm ci --no-audit --prefer-offline --no-progress --timing";
         default.openFiles = [".env"];
+      };
+      onCreate = {
+        firebase-use = "firebase use <project-id>";
       };
   };
   idx.extensions = [
--- nextjs-end/idx-template.nix
+++ nextjs-end/idx-template.nix
@@ -6,8 +6,8 @@
   ];
   bootstrap = ''
     npx -y giget gh:firebase/friendlyeats-web/nextjs-end#jamesdaniels_fixServiceWorkers "$WS_NAME"
-    rm "$WS_NAME/idx-template.json"
-    rm "$WS_NAME/idx-template.nix"
+    rm "$WS_NAME/idx-template".{nix|json}
+    sed -e 's/<project-id>/${projectId}/' ${.idx/dev.nix} > "$WS_NAME/.idx/dev.nix"
     mv "$WS_NAME" "$out"
   '';
 }
