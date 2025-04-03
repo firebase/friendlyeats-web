f1701b2dd2a21aabaf4e1f7fa908770a86c71dce
Jeff Huleatt <3759507+jhuleatt@users.noreply.github.com>
2025-04-03 15:17:09 -0400

    async params

--- nextjs-start/src/app/restaurant/[id]/page.jsx
+++ nextjs-start/src/app/restaurant/[id]/page.jsx
@@ -14,10 +14,11 @@ import {
 } from "@/src/components/Reviews/ReviewSummary";
 import { getFirestore } from "firebase/firestore";
 
-export default async function Home({ params }) {
+export default async function Home(props) {
   // This is a server component, we can access URL
   // parameters via Next.js and download the data
   // we need for this page
+  const params = await props.params;
   const { currentUser } = await getUser();
   const { firebaseServerApp } = await getAuthenticatedAppForUser();
   const restaurant = await getRestaurantById(
